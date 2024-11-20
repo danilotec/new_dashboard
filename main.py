a1e9e9d9db402e36cdf15379945b301bf3b81d06
Danilo Souza <danilo0123499@gmail.com>
2024-11-20 14:23:47 -0300

    resolvendo

--- main.py
+++ main.py
@@ -5,23 +5,26 @@ import pandas as pd
 DATA_BASE = 'data_base.json'
 
 def cadastro():
-    st.title('Cadastro de equipamentos!')
-
-    unidade = st.text_input('Digite a unidade:', key="unidade")
-    equipamento = st.text_input('Digite o novo equipamento:', key="equipamento")
-    n_serie = st.text_input('Digite o numero de serie:', key="n_serie")
-    detalhes = st.text_area(label='Detalhes', placeholder='Descreva sobre o defeito')
-
+    st.subheader('Cadastro de Equipamentos!')
+    col1, col2, col3 = st.columns(3)
     opcoes = ['Cesar', 'Fabricio', 'Thiago']
-    tecnicos = st.multiselect('Selecione um tecnico:', opcoes)
 
-    data_selecionada = st.date_input('Selecione uma data')
+    with col1:
+        unidade = st.text_input('Unidade:', key="unidade")
+        tecnicos = st.multiselect('Selecione um tecnico:', opcoes, placeholder='Selecione uma opção')
+    with col2:    
+        equipamento = st.text_input('Equipamento:', key="equipamento")
+        visita = st.multiselect('Tipo de Visita:', ['Preventiva', 'Corretiva'], placeholder='Selecione um opção')
+    with col3:
+        n_serie = st.text_input('Numero de serie:', key="n_serie")
+        data_selecionada = st.date_input('Selecione uma data')
+
+    detalhes = st.text_area(label='Detalhes:', placeholder='Descreva o defeito')
+    enviar = st.button('Enviar')
+    
     day = data_selecionada.day
     month = data_selecionada.month
 
-    *_, col1 = st.columns(8)
-    with col1:
-        enviar = st.button('enviar')
 
     def carregar_dados():
         if os.path.exists(DATA_BASE):
@@ -37,17 +40,16 @@ def cadastro():
         
         dados_existentes = carregar_dados()
 
-        
         novo_dado = {
             "unidade": unidade,
             "equipamento": equipamento, 
             "numero_serie": n_serie, 
             "detalhes": detalhes,
+            "tipo": visita,
             "tecnicos": tecnicos,
             "data": day,
             "mes": month
         }
-
 
         if isinstance(dados_existentes, list):
             dados_existentes.append(novo_dado)
@@ -65,15 +67,14 @@ def inicio():
     st.subheader('Odontologia')
 
 def tabelas():
-    st.write('Tabelas')
+    st.subheader('Tabelas')
 
     with open(DATA_BASE, 'r') as file:
         data = json.load(file)
-        df = pd.json_normalize(data, 'tecnicos', ['unidade', 'equipamento', 'numero_serie', 'detalhes', 'data', 'mes'], 
+        df = pd.json_normalize(data, 'tecnicos', ['unidade', 'equipamento', 'numero_serie', 'tipo', 'detalhes', 'data', 'mes'], 
                        record_prefix='tecnico_')
         st.dataframe(df)
     
-
 st.sidebar.title('Menu')
 screens = st.sidebar.selectbox(
     'Abas disponiveis:',
